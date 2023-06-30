ff604873d95318361014ba4e40262e1d16ec37bf
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-06-30 23:41:26 +0800

    Added prompts for password criteria. Added code to validate the input.

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,36 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passLength = "";
 
+function generatePassword() {
+  console.log("Button pressed!");
+  passLength = prompt("How many characters would you like your password to contain?");
+  
+// 1. Prompt password criteria
+  
+//  a. Prompt asking the password Length 8 < 128
+  if (passLength <= 7 || passLength >= 129) {
+    passLength = prompt("Try Again. Please enter a number between 8 and 128.");
+  }
+  alert("Your password will have " + passLength + " characters.");
+//  b. Different prompts asking for lowercase, uppercase, numbers or special characers.
+var showLowAlphabet = confirm("Click OK if you would like to include lowercase characters.");
+var showUpAlphabet = confirm("Click OK if you would like to include uppercase characters.");
+var showSpecialChar = confirm("Click OK if you would like to include special characters.");
+var showNumbers = confirm("Click OK if you would like to include numeric characters.");
+
+
+// 2. Validate the input. Ensure everything above.
+
+if (showLowAlphabet === false && showUpAlphabet === false && showSpecialChar === false && showNumbers === false) {
+  alert("You must include at least one lowercase, uppercase, special or numeric character!");
+  showLowAlphabet = confirm("Click OK if you would like to include lowercase characters.");
+  showUpAlphabet = confirm("Click OK if you would like to include uppercase characters.");
+  showSpecialChar = confirm("Click OK if you would like to include special characters.");
+  showNumbers = confirm("Click OK if you would like to include numeric characters.");
+}
+}
 
 // Write password to the #password input
 function writePassword() {
